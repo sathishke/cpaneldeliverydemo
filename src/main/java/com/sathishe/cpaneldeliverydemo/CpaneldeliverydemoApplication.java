36d8b9fecf798d2451f449895be4882c43cf0c49
sathishke <43739573+sathishke@users.noreply.github.com>
2020-03-25 13:39:39 +0530

    initial commit

---
+++ src/main/java/com/sathishe/cpaneldeliverydemo/CpaneldeliverydemoApplication.java
@@ -0,0 +1,13 @@
+package com.sathishe.cpaneldeliverydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpaneldeliverydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpaneldeliverydemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sathishe/cpaneldeliverydemo/CpaneldeliverydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sathishe.cpaneldeliverydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpaneldeliverydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
